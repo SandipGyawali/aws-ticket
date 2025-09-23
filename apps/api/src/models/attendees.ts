179a6262e5c3dec2e4ecfdb7dacfca67ffe2444f
Sandip Gyawali <sandipgyawali10@gmail.com>
2025-09-23 13:33:52 +0545

    session, attendies, user mouels

---
+++ apps/api/src/models/attendees.ts
@@ -0,0 +1,39 @@
+import { pgEnum } from "drizzle-orm/pg-core";
+import { createTable } from "../helpers/create-table";
+import type { ExtractSchema } from "../../@types/db.type";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+
+export const foodPreferenceEnum = pgEnum("food_preference", ["Veg", "NonVeg"]);
+export const sessionChoiceEnum = pgEnum("session_choice", [
+  "Conference",
+  "Workshop",
+]);
+
+const attendees = createTable("user", (t) => ({
+  user_id: t
+    .uuid()
+    .$defaultFn(() => crypto.randomUUID())
+    .primaryKey(),
+  first_name: t.text().notNull(),
+  last_name: t.text().notNull(),
+  phone: t.text(),
+  food_preference: t.text().notNull(),
+  session_choice: t.text().notNull(),
+  checked_in: t.boolean().notNull().default(false),
+  check_in_time: t.timestamp(),
+  lunch: t.boolean().notNull().default(false),
+  lunch2: t.boolean().notNull().default(false),
+  email: t.text().notNull(),
+  created_at: t.timestamp().defaultNow(),
+  updated_at: t
+    .timestamp()
+    .defaultNow()
+    .$onUpdateFn(() => new Date()),
+}));
+
+export type AttendeesSchema = ExtractSchema<typeof attendees>;
+
+const attendeesInsertSchema = createInsertSchema(attendees);
+const attendeesSelectSchema = createSelectSchema(attendees);
+
+export { attendees, attendeesInsertSchema, attendeesSelectSchema };
--- apps/api/src/models/user.ts
+++ apps/api/src/models/user.ts
@@ -3,39 +3,6 @@ import type { ExtractSchema } from "../../@types/db.type";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import crypto from "crypto";
 
-/**
- * -> The above part is for the attendees
- */
-// export enum FoodPreference {
-//   veg = "Veg",
-//   nonVeg = "NonVeg",
-// }
-
-// export enum SessionChoice {
-//   conference = "Conference",
-//   workshop = "Workshop",
-// }
-
-// const user = createTable("user", (t) => ({
-//   user_id: t
-//     .uuid()
-//     .$defaultFn(() => crypto.randomUUID())
-//     .primaryKey(),
-//   first_name: t.text().notNull(),
-//   last_name: t.text().notNull(),
-//   phone: t.text(),
-//   foodPreference: t.text().notNull(),
-//   sessionChoice: t.text().notNull(),
-//   checkedIn: t.boolean().notNull().default(false),
-//   lunch: t.boolean().notNull(),
-//   email: t.text().notNull(),
-//   created_at: t.timestamp().defaultNow(),
-//   updated_at: t
-//     .timestamp()
-//     .defaultNow()
-//     .$onUpdateFn(() => new Date()),
-// }));
-
 const user = createTable("user", (t) => ({
   user_id: t
     .uuid()
